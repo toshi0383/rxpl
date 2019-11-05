bd55d9545a634ce836032a4596ed3368deb4dc67
toshi0383 <t.suzuki326@gmail.com>
2019-11-06 00:57:42 +0900

    fix install by mint

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "rxpl",
+    products: [
+        .executable(name: "rxpl", targets: ["rxpl"]),
+    ],
     dependencies: [
         .package(url: "https://github.com/ReactiveX/RxSwift", from: "5.0.1"),
         .package(url: "https://github.com/jakeheis/SwiftCLI", from: "5.3.3"),
